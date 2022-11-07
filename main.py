19c34c33147c46d36ca11804169d88718a91fad2
MJalingo <113700524+MJalingo@users.noreply.github.com>
2022-11-07 11:04:45 +0100

    stagetwo commit

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    op: str
+    x: int
+    y: int
 
 app = FastAPI()
-
-student = {
-    1:{
-        "name": "Mohammed ",
-        "age": 18,
-        "class":"year 12" 
-        
-    }
-}
 
 @app.get("/")
 def index():
     return  { "slackUsername": "mohammed jalingo nuruddeen", "backend": True, "age": 60, "bio":"python backend developer" }
+
+@app.post('/items/')
+async def do_operation(res: Item):
+    res_dict = res.Dict()
+    if res.op == "addition":
+        result = res.x + res.y
+        res_dict.update({"result": result})
+    elif res.op == "subtraction":
+        result = res.x - res.y
+        res_dict.update({"result": result})
+    elif res.op == "multiplication":
+        result = res.x * res.y
+        res_dict.update({"result": result})
+    return res.dict
